1bc1a07c8a23ed22818aaf880fab0d21a332e464
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2024-11-20 15:36:05 +0600

    Fix age

--- lib/cmed_user.dart
+++ lib/cmed_user.dart
@@ -1,18 +1,18 @@
 class CMEDUser {
   int? id;
   String? gender;
-  int? ageInDays;
+  int? ageInYears;
   int? birthDate;
   double? heightInCm;
   double? weightInKg;
 
-  CMEDUser({this.id = 1, this.gender, this.ageInDays, this.birthDate,
+  CMEDUser({this.id = 1, this.gender, this.ageInYears, this.birthDate,
     this.heightInCm, this.weightInKg});
 
   CMEDUser.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     gender = json['gender'];
-    ageInDays = json['ageInDays'];
+    ageInYears = json['ageInYears'];
     birthDate = json['birthDate'];
     heightInCm = json['heightInCm'];
     weightInKg = json['weightInKg'];
@@ -22,7 +22,7 @@ class CMEDUser {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id??1;
     data['gender'] = gender??"Male";
-    data['ageInDays'] = ageInDays??10585;
+    data['ageInYears'] = ageInYears??18;
     data['birthDate'] = birthDate??0;
     data['heightInCm'] = heightInCm??167.0;
     data['weightInKg'] = weightInKg??60.0;
